212ecfec6aadd1cc4b9a2dfb39d2174f3e4cd67e
kiborgok <borgokalek14@gmail.com>
2020-04-13 13:16:01 +0300

    Initial commit

---
+++ build/precache-manifest.887b0cd26b2d6263bdb0ffddfba26b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e968126c083f3c2461bbff9ef7ad40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d4b26fe39c320d0cfd",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c23d38ac7fe7bdad1755",
+    "url": "/static/css/main.dfc911ec.chunk.css"
+  },
+  {
+    "revision": "68d4b26fe39c320d0cfd",
+    "url": "/static/js/2.b630c688.chunk.js"
+  },
+  {
+    "revision": "c23d38ac7fe7bdad1755",
+    "url": "/static/js/main.6987bdb5.chunk.js"
+  },
+  {
+    "revision": "7a7bd0b67c57a283efaf",
+    "url": "/static/js/runtime~main.6883656c.js"
+  }
+]);
